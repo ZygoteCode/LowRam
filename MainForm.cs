7fb936a6d3fb1c6c2b3759533656432d216df7e2
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-01-21 23:33:35 +0100

    First commit

---
+++ MainForm.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Forms;
+using System.Threading;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System;
+using System.Diagnostics;
+
+public partial class MainForm : Form
+{
+    [DllImport("psapi.dll")]
+    static extern int EmptyWorkingSet(IntPtr hwProc);
+
+    [DllImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool SetProcessWorkingSetSize(IntPtr process, UIntPtr minimumWorkingSetSize, UIntPtr maximumWorkingSetSize);
+
+    public MainForm()
+    {
+        InitializeComponent();
+        new Thread(ClearRAM).Start();
+    }
+
+    public void ClearRAM()
+    {
+        while (true)
+        {
+            Thread.Sleep(750);
+            EmptyWorkingSet(Process.GetCurrentProcess().Handle);
+            GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+            GC.Collect(GC.MaxGeneration);
+            GC.WaitForPendingFinalizers();
+            SetProcessWorkingSetSize(Process.GetCurrentProcess().Handle, (UIntPtr)0xFFFFFFFF, (UIntPtr)0xFFFFFFFF);
+        }
+    }
+
+    private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        Process.GetCurrentProcess().Kill();
+    }
+}
