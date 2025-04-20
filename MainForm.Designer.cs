855c54f4c2329436cef333f96b2ebc6d496873ad
ZygoteCode <zygotecode@proton.me>
2025-04-20 21:42:37 +0200

    First commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,52 @@
+﻿partial class MainForm
+{
+    /// <summary>
+    /// Variabile di progettazione necessaria.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Pulire le risorse in uso.
+    /// </summary>
+    /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Codice generato da Progettazione Windows Form
+
+    /// <summary>
+    /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+    /// il contenuto del metodo con l'editor di codice.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(616, 372);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+
+    }
+
+    #endregion
+}
